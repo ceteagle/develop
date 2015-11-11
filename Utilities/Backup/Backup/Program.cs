4157df00e145450e7a36b28bf46d163d41a57a6b
Eric T <eric.teagle@gmail.com>
2015-11-11 09:14:37 -0500

    Initial checkin of backup utility

---
+++ Utilities/Backup/Backup/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var sourceBaseDirectory = @"\\MYMEDIA\Public\Shared Pictures";
+            var targetBaseDirectory = @"\\MEDIABACKUP\Public\Pictures";
+
+            if (!Directory.Exists(targetBaseDirectory))
+                Directory.CreateDirectory(targetBaseDirectory);
+
+            BackupTool bt = new BackupTool();
+            Console.WriteLine("=== BACKING UP PICTURES ===");
+            bt.RecursiveCopy(sourceBaseDirectory, targetBaseDirectory);
+
+            Console.WriteLine("\n=== BACKING UP VIDEOS ===");
+            sourceBaseDirectory = @"\\MYMEDIA\Public\Shared Videos";
+            targetBaseDirectory = @"\\MEDIABACKUP\Public\Videos";
+            bt.RecursiveCopy(sourceBaseDirectory, targetBaseDirectory);
+
+            bt.Report();
+        }
+    }
+}
