31b987a2e7d266bfabb9f3d7349dcb71328af8f6
ceteagle <eric.teagle@gmail.com>
2017-08-22 12:53:24 -0400

    Add files via upload

---
+++ RW/RWLib/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RWLib
+{
+    public class Character
+    {
+        private Name _name;
+        private Date _dob;
+        private List<Skill> _skills;
+
+        public Character()
+        {
+            _skills = new List<Skill>();
+        }
+
+        public List<Skill> Skills
+        {
+            get
+            {
+                return _skills;
+            }
+        }
+    }
+}
---
+++ RW/RWLib/Skill.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RWLib
+{
+    public enum SkillType
+    {
+        Animals,
+        Artistic,
+        Construction,
+        Cooking,
+        Crafting,
+        Growing,
+        Medicine,
+        Melee,
+        Mining,
+        Intellectual,
+        Shooting,
+        Social,
+        Training
+    }
+
+    public class Skill
+    {
+        private Skill() { }
+
+        public Skill(SkillType type, int level)
+        {
+            Type = type;
+            Level = level;
+        }
+
+        public SkillType Type { get; set; }
+        public int Level { get; set; }
+    }
+}
